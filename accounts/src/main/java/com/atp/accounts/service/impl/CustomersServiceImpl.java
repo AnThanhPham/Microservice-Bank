d20228a87a10d181d15b6f3b7e8e6795fb1db9a7
AnThanhPham <anthanhpham2403@gmail.com>
2025-02-13 09:58:53 +0700

    Feign Client code changes to invoke other microservices

---
+++ accounts/src/main/java/com/atp/accounts/service/impl/CustomersServiceImpl.java
@@ -0,0 +1,47 @@
+package com.atp.accounts.service.impl;
+
+import com.atp.accounts.dto.AccountsDto;
+import com.atp.accounts.dto.CardsDto;
+import com.atp.accounts.dto.CustomerDetailsDto;
+import com.atp.accounts.dto.LoansDto;
+import com.atp.accounts.entity.Accounts;
+import com.atp.accounts.entity.Customer;
+import com.atp.accounts.exception.ResourceNotFoundException;
+import com.atp.accounts.mapper.AccountsMapper;
+import com.atp.accounts.mapper.CustomerMapper;
+import com.atp.accounts.repository.AccountsRepository;
+import com.atp.accounts.repository.CustomerRepository;
+import com.atp.accounts.service.ICustomersService;
+import com.atp.accounts.service.client.CardsFeignClient;
+import com.atp.accounts.service.client.LoansFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomersServiceImpl implements ICustomersService {
+    private final AccountsRepository accountsRepository;
+    private final CustomerRepository customerRepository;
+    private final CardsFeignClient cardsFeignClient;
+    private final LoansFeignClient loansFeignClient;
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
+        );
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString())
+        );
+        CustomerDetailsDto res = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
+        res.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts,new AccountsDto()));
+
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(mobileNumber);
+        res.setCardsDto(cardsDtoResponseEntity.getBody());
+
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(mobileNumber);
+        res.setLoansDto(loansDtoResponseEntity.getBody());
+
+        return res;
+    }
+}
--- loans/src/main/java/com/atp/loans/LoansApplication.java
+++ loans/src/main/java/com/atp/loans/LoansApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -39,6 +40,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 )
 @EnableConfigurationProperties(value = {LoansContactInfoDto.class})
 @EnableDiscoveryClient
+@EnableFeignClients
 public class LoansApplication {
 
 	public static void main(String[] args) {
