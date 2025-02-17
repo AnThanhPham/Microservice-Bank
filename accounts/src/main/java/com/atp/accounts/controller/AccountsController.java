94b2d84968c808681b8d26b15af69c4aa4bb3791
AnThanhPham <anthanhpham2403@gmail.com>
2025-02-17 13:36:58 +0700

    rate limit in account service

--- accounts/src/main/java/com/atp/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/atp/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.atp.accounts.dto.CustomerDto;
 import com.atp.accounts.dto.ErrorResponseDto;
 import com.atp.accounts.dto.ResponseDto;
 import com.atp.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.netty.handler.timeout.TimeoutException;
 import io.swagger.v3.oas.annotations.Operation;
@@ -232,11 +233,18 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name= "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 21");
     }
 
     @Operation(
